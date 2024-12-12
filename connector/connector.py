2f73bf8e67e22785188904454d378ee5b876392c
Sergey Diachkov <78466469+TheSpace-hub@users.noreply.github.com>
2024-12-12 21:20:57 +0300

    Create connect func

--- connector/connector.py
+++ connector/connector.py
@@ -1,7 +1,11 @@
+from socket import socket, AF_INET, SOCK_STREAM
+from data.data import DataIn, DataOut
+
 class Connector:
     def __init__(self, ip: str, port: int):
         self.ip: str = ip
         self.port: int = port
 
     def connect(self):
-        pass
+        s = socket(AF_INET, SOCK_STREAM)
+        s.connect((self.ip, self.port))
