c6aa732f2f76b2afd8edeb4f440e4250bded3943
Sergey Diachkov <78466469+TheSpace-hub@users.noreply.github.com>
2024-12-14 15:45:23 +0300

    Full main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import connector.connector
+from data.data import DataOut
+
+
 def main():
-    pass
+    c = connector.connector.Connector('127.0.0.1', 25570)
+    c.connect()
+    d = DataOut(False, False, False, False, False, False, False, False)
+    while True:
+        c.send_data(d)
+        print(c.get_data().level)
 
 
 if __name__ == '__main__':
